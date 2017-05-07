b5554f350f8e2adf563b0b4ea5cb9c7a921772e4
jahnava <jahnavalmt@gmail.com>
2017-05-07 14:07:18 -0400

    added jquery plugin in head. Added fade functions to three different colored squares

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $('#green').fadeOut(1000);
     $('#yellow').fadeOut(2000);
     $('#red').fadeOut(3000);
+      $('#blue').mouseenter(function() {
+        $('#blue').fadeTo(1000);
+      });
 });
